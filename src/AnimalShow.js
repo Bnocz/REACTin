8da65927653346b07a6c27bf8d7546a9563d6e1d
Bertram Nocon <BertNocon2@gmail.com>
2024-02-18 16:30:45 -0800

    initialized project

---
+++ src/AnimalShow.js
@@ -0,0 +1,23 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird: bird,
+    cat: cat,
+    cow: cow,
+    dog: dog,
+    gato: gator,
+    horse:horse
+};
+function AnimalShow({ type }) {
+    return <div>
+        <img alt="anima" src={svgMap[type] }/>
+    </div>
+}
+
+export default AnimalShow;
