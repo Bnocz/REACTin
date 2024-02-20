3680fefc5e9b62244db1ab7decacfa4d12999c58
Bertram Nocon <BertNocon2@gmail.com>
2024-02-19 19:45:45 -0800

    added fucntionality and styling

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -5,6 +5,7 @@ import dog from './svg/dog.svg';
 import gator from './svg/gator.svg';
 import heart from './svg/heart.svg';
 import horse from './svg/horse.svg';
+import { useState } from 'react';
 
 const svgMap = {
     bird: bird,
@@ -15,9 +16,21 @@ const svgMap = {
     horse:horse
 };
 function AnimalShow({ type }) {
-    return <div>
-        <img alt="anima" src={svgMap[type] }/>
-    </div>
+    const [clicks, setClicks] = useState(0);
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+    return (
+        <div className="animal-show" onClick={handleClick}>
+            <img className="animal" alt="animal" src={svgMap[type]} />
+            <img
+                className="heart"
+                alt="heart"
+                src={heart}
+                style={{ width:10+10 * clicks + 'px' }}
+                />
+        </div>
+    );
 }
 
 export default AnimalShow;
